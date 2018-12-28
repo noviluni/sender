d581e58f2dd7717a29b599952b683857f02b77d2
Marc Hernandez Cabot <noviluni@gmail.com>
2018-12-28 08:25:45 +0000

    send_email improvements
    - Change messages to English
    - Delete **kwargs parameter
    - Add doc to function
    - Encapsulate message generation

--- src/utils.py
+++ src/utils.py
@@ -7,50 +7,68 @@ from email.mime.text import MIMEText
 from conf import GMAIL_USER, GMAIL_PASSWORD, DEFAULT_TO_ADDRESS
 
 
-def send_email(subject, text_message, html_message=None, to_address=DEFAULT_TO_ADDRESS, **kwargs):
-    # Partially based on https://stackoverflow.com/questions/882712/sending-html-email-using-python
+def send_email(subject, text_message, html_message=None, to_address=DEFAULT_TO_ADDRESS):
+    """
+    Given a "subject", a "text_message" and optionally a "html_message" and a "to_address",
+    send an e-mail through gmail smtp.
+    
+    Returns True if e-mail has been sent, else returns False.
+    
+    Partially based on https://stackoverflow.com/questions/882712/sending-html-email-using-python
+    """
 
     try:
+        # Connection with gmail
         smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
         smtpserver.ehlo()
         smtpserver.starttls()
         smtpserver.ehlo()
-        print("Conexion exitosa con Gmail")  # TODO: change to logging
+        print("Connection successful with Gmail")  # TODO: change to logging
 
+        # Login
         try:
             gmail_user = GMAIL_USER
             gmail_pwd = GMAIL_PASSWORD
             smtpserver.login(gmail_user, gmail_pwd)
 
         except smtplib.SMTPException as e:
-            print("Autenticacion incorrecta o intento bloqueado por parte de Google. Error: {}".format(e))
+            print("Incorrect authentication data or attempt blocked by Google
+            Autenticacion incorrecta o intento bloqueado por parte de Google. Error: {}".format(e))
             # TODO: change to logging
             smtpserver.close()
             return False
 
     except (socket.gaierror, socket.error, socket.herror, smtplib.SMTPException) as e:
-        print("Fallo en la conexion con Gmail. Error: {}".format(e))  # TODO: change to logging
+        print("Error connecting to Gmail. Error: {}".format(e))  # TODO: change to logging
         return False
 
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = subject
-    msg['From'] = gmail_user
-    msg['To'] = to_address
+    msg = generate_message(subject, gmail_user, to_address, text_message, html_message)
 
-    part1 = MIMEText(text_message, 'plain')
-    msg.attach(part1)
-
-    if html_message:
-        part2 = MIMEText(html_message, 'html')
-        msg.attach(part2)
-
+    # Send email
     try:
         smtpserver.sendmail(gmail_user, to_address, msg.as_string())
-        print("El correo se envio correctamente")  # TODO: change to logging
+        print("Email has be sent")  # TODO: change to logging
         smtpserver.close()
         return True
 
     except smtplib.SMTPException as e:
-        print("El correo no pudo ser enviado. Error: {}".format(e))  # TODO: change to logging
+        print("Email couldn't be sent. Error: {}".format(e))  # TODO: change to logging
         smtpserver.close()
         return False
+    
+    
+    def generate_message(subject, from_address, to_address, text_message, html_message):
+        """
+        Generates an email (MIMEMultipart object) to be sended.
+        """ 
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = subject
+        msg['From'] = from_address
+        msg['To'] = to_address
+        part1 = MIMEText(text_message, 'plain')
+        msg.attach(part1)
+
+        if html_message:
+            part2 = MIMEText(html_message, 'html')
+            msg.attach(part2)
+        return msg

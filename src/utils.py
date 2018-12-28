81f0020c8297641fcafa9e45c9c7ca1fcedccb31
Marc Hernandez Cabot <noviluni@gmail.com>
2018-12-28 09:10:02 +0000

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -19,27 +19,21 @@ def send_email(subject, text_message, html_message=None, to_address=DEFAULT_TO_A
     
     gmail_user = GMAIL_USER
     gmail_pwd = GMAIL_PASSWORD
-    
+
+    smtpserver = None
 
     try:
         # Connection with gmail
-        smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
-        smtpserver.ehlo()
-        smtpserver.starttls()
-        smtpserver.ehlo()
-        print("Connection successful with Gmail")  # TODO: change to logging
-
-        # Login
+        smtpserver = get_connection()
+        
         try:
-            smtpserver.login(gmail_user, gmail_pwd)
-
+            # Login
+            smtpserver = login(server)
         except smtplib.SMTPException as e:
-            print("Incorrect authentication data or attempt blocked by Google
-            Autenticacion incorrecta o intento bloqueado por parte de Google. Error: {}".format(e))
-            # TODO: change to logging
+            print("Incorrect authentication data or attempt blocked by Google. Error: {}".format(e)) # TODO: change to logging
             smtpserver.close()
             return False
-
+    
     except (socket.gaierror, socket.error, socket.herror, smtplib.SMTPException) as e:
         print("Error connecting to Gmail. Error: {}".format(e))  # TODO: change to logging
         return False
@@ -48,29 +42,41 @@ def send_email(subject, text_message, html_message=None, to_address=DEFAULT_TO_A
 
     # Send email
     try:
-        smtpserver.sendmail(gmail_user, to_address, msg.as_string())
-        print("Email has be sent")  # TODO: change to logging
-        smtpserver.close()
-        return True
-
+        return send(smtpserver, gmail_user, to_address, msg):
     except smtplib.SMTPException as e:
         print("Email couldn't be sent. Error: {}".format(e))  # TODO: change to logging
         smtpserver.close()
         return False
-    
-    
-    def generate_message(subject, from_address, to_address, text_message, html_message):
-        """
-        Generates an email (MIMEMultipart object) to be sended.
-        """ 
-        msg = MIMEMultipart('alternative')
-        msg['Subject'] = subject
-        msg['From'] = from_address
-        msg['To'] = to_address
-        part1 = MIMEText(text_message, 'plain')
-        msg.attach(part1)
 
-        if html_message:
-            part2 = MIMEText(html_message, 'html')
-            msg.attach(part2)
-        return msg
+def get_connection(smtp_host="smtp.gmail.com", port=587):
+    smtpserver = smtplib.SMTP(smtp_host, port)
+    smtpserver.ehlo()
+    smtpserver.starttls()
+    smtpserver.ehlo()
+    print("Connection successful with Gmail")  # TODO: change to logging
+    return smtpserver
+    
+def login(server, user, pwd):
+    smtpserver.login(user, pwd)
+    
+def generate_message(subject, from_address, to_address, text_message, html_message):
+    """
+    Generates an email (MIMEMultipart object) to be sended.
+    """ 
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = from_address
+    msg['To'] = to_address
+    part1 = MIMEText(text_message, 'plain')
+    msg.attach(part1)
+
+    if html_message:
+        part2 = MIMEText(html_message, 'html')
+        msg.attach(part2)
+    return msg
+
+def send(server, from_address, to_address, msg):
+    smtpserver.sendmail(from_address, to_address, msg.as_string())
+    print("Email has been sent")  # TODO: change to logging
+    smtpserver.close()
+    return True

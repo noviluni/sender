5f7697a31f1fb072a3c61c3722af188cea8dc34b
Marc Hernandez Cabot <noviluni@gmail.com>
2018-12-28 08:13:57 +0000

    Fix send method and change some defaults to serve_default

--- src/models.py
+++ src/models.py
@@ -12,7 +12,7 @@ class Email(db.Model):
     __tablename__ = 'emails'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-
+    
     from_address = db.Column(db.String(200), nullable=False)  # TODO: Add email validation
     to_address = db.Column(db.String(200), nullable=False, default=DEFAULT_TO_ADDRESS)  # TODO: Add email validation
 
@@ -21,27 +21,35 @@ class Email(db.Model):
     html_message = db.Column(db.String(200))  # TODO: Define bigger body?
 
     created_at = db.Column(db.DateTime, nullable=False)
-    sent = db.Column(db.Boolean(), default=False, nullable=False)
+    sent = db.Column(db.Boolean(), server_default=False, nullable=False)
     sent_at = db.Column(db.DateTime)
-    retries = db.Column(db.Integer, default=0)
+    retries = db.Column(db.Integer, server_default=0)
 
     def __repr__(self):
         return '<Email {} - {}: {}>'.format(self.id, self.to_address, self.subject)
 
-    def __init__(self, from_address, to_address, subject, text_message, html_message):
+    def __init__(self, from_address, to_address, subject, text_message, html_message, sent=False):
         self.from_address = from_address  # TODO: Add email validation
         self.to_address = to_address  # TODO: Add email validation
         self.subject = subject
         self.text_message = text_message
         self.html_message = html_message
         self.created_at = datetime.datetime.now()
+        self.sent = sent
 
-    def send(self):
-        sent = send_email(**self.__dict__)
+    def send(self, fnx=send_email):
         self.retries += 1
+        email_data = self.get_email_data()
+        sent = fnx(**email_data)
 
         if sent:
             self.sent = True
             self.sent_at = datetime.datetime.now()
-        db.session.commit()
         return sent
+    
+    def get_email_data(self):
+        email_data = {'subject': self.subject,
+                      'text_message': self.text_message,
+                      'html_message': self.html_message,
+                      'to_address': self.to_address}
+        return email_data
